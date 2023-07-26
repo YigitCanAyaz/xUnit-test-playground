0a94857edbc3a63fc48a596e9e0dfdf76f80db77
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-07-26 20:05:41 +0300

    Add project files.

---
+++ XUnitTestBase.Test/CalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using XUnitTestBase.APP;
+
+namespace XUnitTestBase.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            // Act
+            var total = calculator.Add(a, b);
+
+            // Assert
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
